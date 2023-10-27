468b9fe95b0f64c841577b544c8b562777934be5
seqyuan <ahworld07@gmail.com>
2023-10-27 15:12:55 +0800

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -4,11 +4,9 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
-git remote add mirror "$INPUT_TARGET_REPO_URL"
 git clone https://github.com/seqyuan/sphinxbook.git
 cp -r sphinxbook/docs/* ./docs/
-git add -A
-git commit -m "..."
+git remote add mirror "$INPUT_TARGET_REPO_URL"
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
