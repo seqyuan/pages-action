0d500d7024d029cbb43574304af4115f57e92c87
seqyuan <ahworld07@gmail.com>
2023-10-27 15:33:29 +0800

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -4,8 +4,13 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+git config --global user.name 'seqyuan'
+git config --global user.email 'ahworld07@gmail.com'
 git clone https://github.com/seqyuan/sphinxbook.git
 cp -r sphinxbook/docs/* ./docs/
+git add -A
+git commit -m "..."
+
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
 
